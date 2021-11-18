60416322f20d519616a2c243b2a4c6d57e12607c
dmkrupin <dev.dmkrupin@gmail.com>
2021-11-18 18:25:24 +0300

    заменил значение по умолчанию в функции debounce на глобальную переменную

--- js/utils/debounce.js
+++ js/utils/debounce.js
@@ -1,7 +1,9 @@
+import { RERENDER_DELAY } from '../global-variables.js';
+
 // Функция взята из интернета и доработана
 // Источник - https://www.freecodecamp.org/news/javascript-debounce-example
 
-function debounce (callback, timeoutDelay = 500) {
+function debounce (callback, timeoutDelay = RERENDER_DELAY) {
   // Используем замыкания, чтобы id таймаута у нас навсегда приклеился
   // к возвращаемой функции с setTimeout, тогда мы его сможем перезаписывать
   let timeoutId;
